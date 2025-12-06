dbe0f8df404ce979af3cf870c9d11c499f3bdc85
Jhanu-FSD <jhansig30fsd@gmail.com>
2025-12-06 22:00:17 +0530

    Task 3 - Hover square to circle assignment

---
+++ Tasks/Task 3 - Hover Square-to-Circle Transformation (JS)/Jhansi_G30 FSD/script.js
@@ -0,0 +1,9 @@
+const box = document.getElementById("box");
+
+box.addEventListener("mouseover", () => {
+  box.style.borderRadius = "50%";
+});
+
+box.addEventListener("mouseout", () => {
+  box.style.borderRadius = "0";
+});
